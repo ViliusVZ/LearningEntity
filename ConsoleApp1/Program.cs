84811538afbbbcd5062ac219c76100d01ee73a29
ViliusVZ <vilius.zobela@gmail.com>
2018-12-28 18:55:08 +0200

    Major changes. Started working on console application.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertNinja();
+            Console.ReadLine();
+        }
+
+        private static void InsertNinja()
+        {
+            var ninja = new Ninja
+            {
+                Name = "JulieSan",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1980, 1, 1),
+                ClanId = 1
+            };
+        }
+    }
+}
